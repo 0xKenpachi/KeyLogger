5866e4faa6798fa1b20bd63ca98b967c5d57e2fd
0xKenpachi <laupermathieu@gmail.com>
2024-10-06 18:56:57 +0200

    Initial Commit

---
+++ test.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MonProjetCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour, monde!");
+        }
+    }
+}
