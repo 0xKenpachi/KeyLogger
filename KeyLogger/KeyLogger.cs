ec4fdf6fd0cefaf9d1a80cd4ae22c60ec9520b44
0xKenpachi <laupermathieu@gmail.com>
2024-10-06 19:17:36 +0200

    Keylogger first version

---
+++ KeyLogger/KeyLogger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MonProjetCSharp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Créer un token d'annulation pour détecter CTRL + C
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true; // Empêche l'application de se fermer immédiatement
+                cts.Cancel();
+            };
+
+            // Boucle infinie pour lire l'entrée de l'utilisateur
+            while (true)
+            {
+                try
+                {
+                    // Demander à l'utilisateur d'entrer du texte
+                    Console.WriteLine("Veuillez entrer du texte (appuyez sur CTRL + C pour quitter) :");
+
+                    // Lire l'entrée de l'utilisateur et la stocker dans une variable
+                    string input = await Task.Run(() => Console.ReadLine(), cts.Token);
+
+                    // Afficher l'entrée de l'utilisateur
+                    Console.WriteLine("Vous avez entré : " + input);
+                }
+                catch (OperationCanceledException)
+                {
+                    // L'utilisateur a appuyé sur CTRL + C
+                    Console.WriteLine("Programme arrêté par l'utilisateur.");
+                    break;
+                }
+            }
+        }
+    }
+}
